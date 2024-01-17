34f0e3e6229dde115aaefef8f688876f7b9e798f
Odorizzi1 <88588959+Odorizzi1@users.noreply.github.com>
2024-01-16 21:03:23 -0300

    Create form structure

--- client/src/pages/student-form/StudentFormView.tsx
+++ client/src/pages/student-form/StudentFormView.tsx
@@ -1,7 +1,35 @@
-const StudentFormView = ()=>{
-    return(
-        <div> teste123</div>
-    )
-}
+import { Box } from "@mui/material";
+import { Button, TextField } from "../../components/atoms";
 
-export {StudentFormView}
+const StudentFormView = () => {
+  return (
+    <Box>
+      <Box
+        width={"100vw"}
+        height={"100vh"}
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"center"}
+      >
+        <Box  display={"flex"} flexDirection={"column"} gap={"30px"}>
+          <Box gap={"20px"} width={"100%"} display={"flex"}>
+            <TextField variant="standard" />
+            <TextField variant="standard" />
+          </Box>
+          <Box gap={"20px"} width={"100%"} display={"flex"}>
+            <TextField variant="standard" />
+            <TextField variant="standard" />
+          </Box>
+          <Box width={"100%"}>
+            <Button>Cadastrar aluno</Button>
+          </Box>
+        </Box>
+        <Box height={"475px"}>
+          <img src="/cuate.svg" />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export { StudentFormView };
