a0c5b09b42d871ec7e34059d96e2a2d214c1ed74
Odorizzi1 <88588959+Odorizzi1@users.noreply.github.com>
2024-01-28 21:40:28 -0300

    insert react-hook-from and logics

--- client/src/components/molecules/login/LoginFields.tsx
+++ client/src/components/molecules/login/LoginFields.tsx
@@ -1,35 +1,61 @@
 import { Box } from "@mui/material";
 import { Button, TextField } from "../../atoms";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+
+type InputLogin = {
+  user: string;
+  password: string;
+};
 
 const LoginFields = () => {
+  const { handleSubmit, control } = useForm<InputLogin>();
+
+  const onSubmit: SubmitHandler<InputLogin> = (data) => {};
+
   return (
-    <Box
-      width="423px"
-      height="382px"
-      borderRadius="10px"
-      boxShadow="3px 3px 10px 1px rgba(0, 0, 0, 0.25);"
-      display="flex"
-      alignItems="center"
-      justifyContent="space-around"
-      flexDirection="column"
-    >
+    <form onSubmit={handleSubmit(onSubmit)}>
       <Box
-        gap="10px"
+        width="423px"
+        height="382px"
+        borderRadius="10px"
+        boxShadow="3px 3px 10px 1px rgba(0, 0, 0, 0.25);"
         display="flex"
+        alignItems="center"
+        justifyContent="space-around"
         flexDirection="column"
-        width="323px"
-        height="38px"
       >
-        <TextField placeholder="E-mail" />
+        <Box
+          gap="10px"
+          display="flex"
+          flexDirection="column"
+          width="323px"
+          height="38px"
+        >
+          <Controller
+            name="user"
+            control={control}
+            defaultValue=""
+            render={({ field }) => (
+              <TextField {...field} placeholder="E-mail" />
+            )}
+          />
 
-        <TextField placeholder="Senha" type="password" />
-        {/* Criar TextField de senha a parte com manipulações de state e valores nele separado */}
-      </Box>
+          <Controller
+            name="password"
+            control={control}
+            defaultValue=""
+            render={({ field }) => (
+              <TextField {...field} placeholder="Senha" type="password" />
+            )}
+          />
+        </Box>
 
-      <Box width="323px" height="43px">
-        <Button> Login </Button>
+        <Box width="323px" height="43px">
+          <Button type="submit">Login</Button>
+        </Box>
       </Box>
-    </Box>
+    </form>
   );
 };
+
 export { LoginFields };
