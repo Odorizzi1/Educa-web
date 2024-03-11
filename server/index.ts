aca7852b624203ae26597933f3f14c16c5aa6ed2
Odorizzi1 <88588959+Odorizzi1@users.noreply.github.com>
2024-03-11 19:35:28 -0300

    update

--- server/index.ts
+++ server/index.ts
@@ -6,11 +6,34 @@ import router from "./routes/routes";
 
 import bodyParser from "body-parser";
 import { connectDb } from "./data/database/ConectionDatabase";
-
+import { register, collectDefaultMetrics, Counter } from "prom-client";
 dotenv.config();
 const PORT = process.env.PORT || 3000;
-
 const app = express();
+collectDefaultMetrics();
+
+const requestCounter = new Counter({
+  name: "myapp_requests_total",
+  help: "Número total de requisições ao aplicativo.",
+});
+
+app.get("/metrics", async (req, res) => {
+  try {
+    res.set("Content-Type", register.contentType);
+    const metrics = await register.metrics();
+    res.end(metrics);
+  } catch (error) {
+    console.error("Erro ao obter métricas:", error);
+    res.status(500).send("Erro ao obter métricas");
+  }
+});
+
+app.get("/teacher", (req, res) => {
+  // Incrementar a métrica de contagem de requisições
+  requestCounter.inc();
+
+  // Outras operações do seu aplicativo
+});
 
 app.use(cors());
 
